c3c5fd7754e568373821ae391d7436f60a28dec8
veryyoung <codingyoung@gmail.com>
2016-01-29 21:54:45 +0800

    manage the project with gradle

---
+++ src/main/java/me/veryyoung/oj/leetcode/ThreeSum.java
@@ -0,0 +1,45 @@
+package me.veryyoung.oj.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: veryyoung
+ * Email:codingyoung@gmail.com
+ * Date: 14-4-12
+ * Time: 下午11:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class ThreeSum {
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+        ArrayList<ArrayList<Integer>> returnArray = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> eachArray;
+        int length = num.length;
+        for (int i = 0; i < length; i++) {
+            int target = 0 - num[i];
+            int start = i + 1, end = length - 1;
+            while (start < end) {
+                if (num[start] + num[end] == target) {
+                    eachArray = new ArrayList<Integer>(3);
+                    eachArray.add(0, num[i]);
+                    eachArray.add(1, num[start]);
+                    eachArray.add(2, num[end]);
+                    returnArray.add(eachArray);
+                    start++;
+                    end--;
+                    while (start < end && num[start] == num[start - 1]) start++;
+                    while (start < end && num[end] == num[end + 1]) end--;
+                } else if (num[start] + num[end] < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+            while (i < length - 1 && num[i] == num[i + 1]) i++;
+
+        }
+        return returnArray;
+    }
+}
---
+++ src/main/java/me/veryyoung/oj/leetcode/TwoSum.java
@@ -0,0 +1,34 @@
+package me.veryyoung.oj.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: veryyoung
+ * Email:codingyoung@gmail.com
+ * Date: 14-4-12
+ * Time: 上午12:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int length = numbers.length;
+        if (length < 2) return null;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] returnNums = new int[2];
+        for (int i = 0; i < length; i++) {
+            if (!map.containsKey(numbers[i])) {
+                map.put(target - numbers[i], i);
+            } else {
+                int index = map.get(numbers[i]);
+                if (index < i) {
+                    returnNums[0] = index + 1;
+                    returnNums[1] = i + 1;
+                }
+            }
+        }
+        return returnNums;
+    }
+}
