91c0794e78dd2be3184f8f9a372dcb14d40fe3d8
veryyoung <codingyoung@gmail.com>
2016-02-01 15:26:06 +0800

    原串翻转

---
+++ src/test/java/me/veryyoung/oj/cc150/StringReverseTest.java
@@ -0,0 +1,21 @@
+package me.veryyoung.oj.cc150;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by yangxiongwei on 2016/2/1.
+ */
+public class StringReverseTest {
+
+    @Test
+    public void testReverseString() {
+        StringReverse stringReverse = new StringReverse();
+        Assert.assertEquals(null, stringReverse.reverseString(null));
+        Assert.assertEquals("", stringReverse.reverseString(""));
+        Assert.assertEquals("a", stringReverse.reverseString("a"));
+        Assert.assertEquals("aeiou", stringReverse.reverseString("uoiea"));
+        Assert.assertEquals("redocwon si sihT", stringReverse.reverseString("This is nowcoder"));
+
+    }
+}
