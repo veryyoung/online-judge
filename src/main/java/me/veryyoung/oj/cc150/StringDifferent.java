422f4e922ccfb1990ead386a68d98198b2c2b364
veryyoung <codingyoung@gmail.com>
2016-02-01 15:11:40 +0800

    use array instead of map

--- src/main/java/me/veryyoung/oj/cc150/StringDifferent.java
+++ src/main/java/me/veryyoung/oj/cc150/StringDifferent.java
@@ -1,8 +1,5 @@
 package me.veryyoung.oj.cc150;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * 请实现一个算法，确定一个字符串的所有字符是否全都不同。这里我们要求不允许使用额外的存储结构。
  * 给定一个string iniString，请返回一个bool值,True代表所有字符全都不同，False代表存在相同的字符。保证字符串中的字符为ASCII字符。字符串的长度小于等于3000。
@@ -16,17 +13,18 @@ import java.util.Map;
 public class StringDifferent {
 
     public boolean checkDifferent(String iniString) {
-        if (iniString == null || iniString.length() < 2) {
+        if (iniString == null || iniString.length() < 2 || iniString.length() > 256) {
             return false;
         }
 
-        Map<Character, Boolean> charExistMap = new HashMap<>();
+        boolean[] charExistArray = new boolean[256];
+
         for (int i = 0; i < iniString.length(); i++) {
             char c = iniString.charAt(i);
-            if (charExistMap.get(c) != null) {
+            if (charExistArray[c]) {
                 return false;
             } else {
-                charExistMap.put(c, true);
+                charExistArray[c] = true;
             }
         }
         return true;
