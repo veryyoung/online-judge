1084acea3e07dc34078218af63039848a578846a
veryyoung <codingyoung@gmail.com>
2016-02-03 17:10:41 +0800

    FindKthToTail

---
+++ src/main/java/me/veryyoung/oj/cc150/ListNode.java
@@ -0,0 +1,15 @@
+package me.veryyoung.oj.cc150;
+
+/**
+ * Created by veryyoung on 2016/2/3.
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
