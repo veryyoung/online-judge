5430aafc1840b9b23d529c2b60260e02f591a620
veryyoung <veryyoung.me@gmail.com>
2023-07-13 22:50:41 +0900

    Set diffCount as param

--- app/src/main/java/me/veryyoung/oj/leetcode/ClosestDivisors.java
+++ app/src/main/java/me/veryyoung/oj/leetcode/ClosestDivisors.java
@@ -1,5 +1,8 @@
 package me.veryyoung.oj.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 1362. Closest Divisors
  * https://leetcode.com/problems/closest-divisors/
@@ -7,30 +10,30 @@ package me.veryyoung.oj.leetcode;
 public class ClosestDivisors {
 
     public int[] closestDivisors(int num) {
-        int firstSmaller = 0;
-        int firstBigger = 0;
-        int secondSmaller = 0;
-        int secondBigger = 0;
-        for (int i = (int) Math.sqrt(num + 1); i > 0; i--) {
-            if ((num + 1) % i == 0) {
-                firstBigger = (num + 1) / i;
-                firstSmaller = i;
-                break;
-            }
-        }
-        for (int i = (int) Math.sqrt(num + 2); i > 0; i--) {
-            if ((num + 2) % i == 0) {
-                secondBigger = (num + 2) / i;
-                secondSmaller = i;
-                break;
+        int diffCount = 2;
+        Map<Integer, Integer> multipliers = new HashMap<>(diffCount);
+        for (int count = diffCount; count > 0; count--) {
+            for (int i = (int) Math.sqrt(num + count); i > 0; i--) {
+                if ((num + count) % i == 0) {
+                    multipliers.put((num + count) / i, i);
+                    break;
+                }
             }
         }
 
-        if ((firstBigger - firstSmaller) <= (secondBigger - secondSmaller)) {
-            return new int[]{firstSmaller, firstBigger};
-        } else {
-            return new int[]{secondSmaller, secondBigger};
+        int smallest = Integer.MAX_VALUE;
+        int first = 0;
+        int second = 0;
+        for (Integer bigger : multipliers.keySet()) {
+            int smaller = multipliers.get(bigger);
+            int diff = bigger - smaller;
+            if (smallest > diff) {
+                smallest = diff;
+                first = smaller;
+                second = bigger;
+            }
         }
+        return new int[]{first, second};
     }
 
 }
