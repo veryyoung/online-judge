086f8a2846a16f1a03cbbd025a29360ac562179b
veryyoung <veryyoung.me@gmail.com>
2023-08-28 23:19:23 +0900

    Add comment for TrappingRainWater

--- app/src/main/java/me/veryyoung/oj/leetcode/TrappingRainWater.java
+++ app/src/main/java/me/veryyoung/oj/leetcode/TrappingRainWater.java
@@ -6,6 +6,13 @@ package me.veryyoung.oj.leetcode;
  */
 public class TrappingRainWater {
 
+    /**
+     * The amount of water for height[i] is determined by the left max height and right max height
+     * water[i] = min(max(height[0..i]), max(height[i..end])) - height[i]
+     * Use two numbers leftMax and leftMax to store the max part, and two pointer left and right two traversal the arr
+     * If height[left] is smaller than height[right], if height[left] bigger than leftMax, replace the leftMax, else, count the water
+     * Else  if height[right] bigger than rightMax,  replace the rightMax, else count the water
+     */
     public int trap(int[] height) {
         int left = 0;
         int right = height.length - 1;
