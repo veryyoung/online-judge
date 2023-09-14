e761a86a56cdb013637ee27cc28fb9b5b4e21e10
veryyoung <veryyoung.me@gmail.com>
2023-09-15 00:04:00 +0900

    Fix twosum

--- app/src/main/java/me/veryyoung/oj/leetcode/TwoSum.java
+++ app/src/main/java/me/veryyoung/oj/leetcode/TwoSum.java
@@ -10,17 +10,16 @@ import java.util.Map;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        if (nums.length < 2) return null;
-        Map<Integer, Integer> map = new HashMap<>();
+        if (nums.length < 2) {
+            return null;
+        }
+        Map<Integer, Integer> numToIndex = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(target - nums[i], i);
-            } else {
-                int index = map.get(nums[i]);
-                if (index < i) {
-                    return new int[]{index + 1, i + 1};
-                }
+            int left = target - nums[i];
+            if (numToIndex.containsKey(left)) {
+                return new int[]{numToIndex.get(left), i};
             }
+            numToIndex.put(nums[i], i);
         }
         return null;
     }
--- app/src/test/java/me/veryyoung/oj/leetcode/TwoSumTest.java
+++ app/src/test/java/me/veryyoung/oj/leetcode/TwoSumTest.java
@@ -6,12 +6,12 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class TwoSumTest {
 
-
     @Test
     public void testTwoSum() {
         TwoSum instance = new TwoSum();
-        assertArrayEquals(new int[]{1, 2}, instance.twoSum(new int[]{2, 7, 11, 15}, 9));
+        assertArrayEquals(new int[]{0, 1}, instance.twoSum(new int[]{2, 7, 11, 15}, 9));
         assertArrayEquals(new int[]{1, 2}, instance.twoSum(new int[]{3, 2, 4}, 6));
         assertArrayEquals(new int[]{0, 1}, instance.twoSum(new int[]{3, 3}, 6));
     }
+
 }
